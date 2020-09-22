f0ad348acab598850e9c0d07d41d6cc97ae0a8d3
Kenneth Transier <ktransier@gmail.com>
2020-09-22 00:39:39 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Runway::Engine.routes.draw do
-	root to: 'pages#home'
-	get 'home',    to: 'pages#home'
-	get 'privacy', to: 'pages#privacy_policy'
+#   root to: 'pages#home'
+#   get 'home',    to: 'pages#home'
+  get 'privacy', to: 'pages#privacy_policy'
   get 'support', to: 'pages#support'
 end
